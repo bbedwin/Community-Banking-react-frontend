ac28d16fe43743e68ac9828e87605a563c660331
unknown <you@example.com>
2023-05-04 16:37:17 +0530

    fix repayment schedule

--- src/pages/loan_repayment_schedule/loan_repayment_schedule.jsx
+++ src/pages/loan_repayment_schedule/loan_repayment_schedule.jsx
@@ -6,7 +6,20 @@ import NavigationBar from '../../components/NavigationBar/NavigationBar'
 import { toast } from 'react-toastify'
 import './loan_repayment_schedule.css'
 
+import { Modal } from "react-bootstrap";
+
 const LRepaymentSchedule = () => {
+    const [Rdata, setData] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => {
+        setShowModal(true);
+      };
+    
+      const closeModal = () => {
+        setShowModal(false);
+      };
+
     const { authToken, userInfo } = useContext(ProjectContext)
     const navigate = useNavigate()
 
@@ -27,10 +40,12 @@ const LRepaymentSchedule = () => {
                 "interest_rate": loanIntrestrate,
             })
 
-            if (response.status == 201) {
+            if (response.status === 201) {
                 toast.success("Repayment Scheduled successfully")
                 navigate('/loanrepaymentschedule')
             }
+            console.log(response.data)
+            setData(response.data);
 
         } catch (error) {
             if (error.response.data.message == "Please complete your profile to proceed") {
@@ -75,11 +90,11 @@ const LRepaymentSchedule = () => {
 
                             <div className="mb-3">
                                 <label htmlFor="frequency" className="form-label">Frequency</label>
-                                <select className="form-select" id='frequency' name='Frequency'
+                                <select className="form-select" id='frequency' name='frequency'
                                     onChange={(e) => { setLoanFrequency(e.target.value) }}>
-                                    <option value="daily">Daily</option>
+                                    <option value="daily" >Daily</option>
                                     <option value="weekly">Weekly</option>
-                                    <option value="montly">Montly</option>
+                                    <option value="monthly">Monthly</option>
                                     <option value="quarterly">Quaterly</option>
                                     <option value="half_yearly">Half Yearly</option>
                                     <option value="annualy">Annualy</option>
@@ -91,13 +106,47 @@ const LRepaymentSchedule = () => {
                             </div>
 
                             <div className="text-center">
-                                <button type="submit" className="btn btn-success">Save</button>
+                                {/* <button type="submit" className="btn btn-success">View Schedules</button> */}
+                                <button  type="submit" className="btn btn-primary" onClick={openModal}>
+            View Repayment Schedule
+          </button>
                             </div>
-                        </form>
+                           
+                        </form><br />
+                        <Modal show={showModal} onHide={closeModal} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Repayment Schedule</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Repayment Date</th>
+                <th>Repayment Amount</th>
+                <th>Principal Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Rdata.map((item, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{item.repayment_date}</td>
+                  <td>{item.repayment_amount}</td>
+                  <td>{item.principal_amount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Modal.Body>
+      </Modal>
+                   </div>
                     </div>
+                  
+
                 </div>
             </div>
-        </div>
+       
     )
 }
 
